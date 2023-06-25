5907a76c60ace9885222fa5bfa8cce6a82334a55
Barnabas Kendall <barnabas.kendall@gmail.com>
2023-06-25 07:57:13 -0400

    more analytics on router

--- src/routes.ts
+++ src/routes.ts
@@ -5,12 +5,16 @@ import Section from "./pages/SectionPage.vue";
 import Level from "./pages/LevelPage.vue";
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/:section', component: Section, props: true, },
-  { path: '/:section/:level', component: Level, props: true, },
-]
+  { path: "/", component: Home },
+  { path: "/:section", component: Section, props: true },
+  { path: "/:section/:level", component: Level, props: true },
+];
 export const router = createRouter({
   history: createWebHashHistory(),
   routes,
-})
+});
 
+router.afterEach((to, _from, failure) => {
+  const analytics = (<any>window).beam;
+  if (!failure && typeof analytics === "function") analytics(to.fullPath);
+});

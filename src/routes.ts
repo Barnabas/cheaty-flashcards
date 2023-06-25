3f7063831e4ed8229513ab7058cbc730d1a3e971
Barnabas Kendall <barnabas.kendall@gmail.com>
2023-06-24 23:26:31 -0400

    remove about page

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "./pages/HomePage.vue";
-import About from "./pages/AboutPage.vue";
 import Section from "./pages/SectionPage.vue";
 import Level from "./pages/LevelPage.vue";
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/about', component: About },
   { path: '/:section', component: Section, props: true, },
   { path: '/:section/:level', component: Level, props: true, },
 ]

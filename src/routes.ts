c0acec261fefd2cfaaa45573be52bbaba90d4a86
Barnabas Kendall <barnabas.kendall@gmail.com>
2026-07-19 16:01:48 -0400

    remove analytics

--- src/routes.ts
+++ src/routes.ts
@@ -13,8 +13,3 @@ export const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
-
-router.afterEach((to, _from, failure) => {
-  const analytics = (<any>window).beam;
-  if (!failure && typeof analytics === "function") analytics(to.fullPath);
-});
